e4dc62056b988aff027ed7c16c238d47354e105a
john-igaw <john@igaworks.com>
2017-07-03 10:16:29 +0900

    stable version

--- IgaworksLiveOps3rd.framework/Versions/A/Headers/IgaworksLiveOps3rdPush.h
+++ IgaworksLiveOps3rd.framework/Versions/A/Headers/IgaworksLiveOps3rdPush.h
@@ -1,33 +1,37 @@
 //
-//  LiveOpsPush.h
-//  LiveOps
+//  IgaworksLiveOps3rdPush.h
+//  IgaworksLiveOps3rd
 //
-//  Created by 강기태 on 2014. 7. 29..
+//  Created by John on 2017. 6. 30..
 //  Copyright (c) 2014년 IGAWorks. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_10_0
 #import <UserNotifications/UserNotifications.h>
+#else
 
+#endif
 
-@interface LiveOpsPushInfo : NSObject
+@interface IgaworksLiveOps3rdPushInfo : NSObject
 
-@property (nonatomic,copy) NSDate* sentTime;
-@property (nonatomic,copy) NSString* bodyText;
-@property (nonatomic,copy) NSString* deepLinkUrl;
-@property (nonatomic,copy) id deepLink;
+@property (nonatomic,copy) NSDate* IgaworksLiveOps3rd_sentTime;
+@property (nonatomic,copy) NSString* IgaworksLiveOps3rd_BodyText;
+@property (nonatomic,copy) NSString* IgaworksLiveOps3rd_DeepLinkUrl;
+@property (nonatomic,copy) id IgaworksLiveOps3rd_deepLink;
 
 @end
-
-
 
 typedef void (^LiveOpsLocalNotificationCallback)(NSInteger Id, NSDate* sentTime, NSString* bodyText, NSDictionary* customData, BOOL isForeGround);
 typedef void (^LiveOpsRemoteNotificationCallback)(NSArray* pushInfos, BOOL isForeGround);
 typedef void (^LiveOpsRemotePushEnableCallback)(BOOL result);
 
-
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_10_0
 @interface IgaworksLiveOps3rdPush : NSObject <UNUserNotificationCenterDelegate>
+#else
+@interface IgaworksLiveOps3rdPush : NSObject
+#endif
 
 + (void)init3rdPush;
 + (void)setDeviceToken:(NSData*)deviceToken;
